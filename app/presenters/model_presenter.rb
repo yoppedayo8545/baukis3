6439a0d17b258b3e0dd29f55cccf1901fe5554f9
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-10-26 14:15:41 +0900

    入力フォームとerror_messageの追加
    HtmlBuilder用

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -8,4 +8,33 @@ class ModelPresenter
     @object = object
     @view_context = view_context
   end
+
+  def notes
+    markup(:div, class: "notes") do |m|
+      m.span "*", class: "mark"
+      m.text "印の追加項目は入力必須です。"
+    end
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name, label_text, options)
+      m << text_field(name, options)
+      m << errors_messages_for(name)
+    end
+  end
+
+  def errors_messages_for(name)
+    markup do |m|
+      object.errors.full_messages_for(name).each do |message|
+        m.div(class: "error-message") do |m|
+          m.text message
+        end
+      end
+    end
+  end
+
+  private def decorated_label(name, label_text, options = {})
+    label(name, label_text, class: options[:required] ? "required" : nil )
+  end
 end
