0a736a0012c837f30c55027f5dbc1483e9a44c86
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-10-31 23:03:23 +0900

    Customer用migrationファイル

---
+++ db/migrate/20211026052404_create_customers.rb
@@ -0,0 +1,18 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :email, null: false #メールアドレス
+      t.string :family_name, null: false #性
+      t.string :given_name, null: false #名
+      t.string :family_name_kana, null: false #性(カナ)
+      t.string :given_name_kana, null: false #名(カナ)
+      t.string :gender #性別
+      t.date :birthday #誕生日
+      t.string :hashed_password #パスワード
+
+      t.timestamps
+    end
+    add_index :customers, "LOWER(email)", unique: true
+    add_index :customers, [:family_name_kana, :given_name_kana]
+  end
+end
