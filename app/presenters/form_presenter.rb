4b1ea011ce826e66d6989276ab831afb3e421894
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-11-04 14:06:09 +0900

    誤字修正

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -45,7 +45,7 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << form_builder.select(name, choices, {include_blank: true}, options)
-      m << error_messages_for(name)
+      m << errors_messages_for(name)
     end
   end
   
